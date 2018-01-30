8813a1cdbbaa751b2d9e9976478836679dc0d38a
Adriana Becerril <Adriana@MacBook-Air-de-Arisbeth.local>
2018-01-30 00:47:08 -0600

    corrgiendo header y agregando avatar de usuario

--- js/app.js
+++ js/app.js
@@ -5,41 +5,40 @@ var $submit = $("#publish");
 var $imagePublish = $("#image-publish");
 var $comment = $("#comment");
 
-function loadPage () {
-  $("#menu-bars").click(menu);
-  $submit.click(showSugg);
-  
+function loadPage() {
+    $("#menu-bars").click(menu);
+    $submit.click(showSugg);
+
 }
 
-  function menu () {
+function menu() {
     if ($(".options").hasClass("hidden")) {
-          $(".options").removeClass("hidden");
-          $(".options").addClass("show");
-      } else {
+        $(".options").removeClass("hidden");
+        $(".options").addClass("show");
+    } else {
         $(".options").removeClass("show");
         $(".options").addClass("hidden");
-      }
     }
+}
 
-function showSugg () {
-  var $textUser = $("#text-user");
-  var $textUserVal = $textUser.val();
-  $textUser.val(" ");
+function showSugg() {
+    var $textUser = $("#text-user");
+    var $textUserVal = $textUser.val();
+    $textUser.val(" ");
 
-  var d = new Date();
-  var n = d.toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit"
-  });
+    var d = new Date();
+    var n = d.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit"
+    });
 
-   var time = $("#time").text(Date());
-   $comment.append(time);
+    var time = $("#time").text(Date());
+    $comment.append(time);
 
 
-  console.log(time);
+    console.log(time);
 
-} 
+}
 
 
 $(document).ready(loadPage);
-
